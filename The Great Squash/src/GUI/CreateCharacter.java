78e65cdc5f1522f2b8212b960d91401794a0efea
ros_dmlamarca <ros_dmlamarca@WROSRM31922.seattleschools.org>
2014-12-05 09:44:36 -0800

    I worked on CreareCharacter and stuff

--- The Great Squash/src/GUI/CreateCharacter.java
+++ The Great Squash/src/GUI/CreateCharacter.java
@@ -1,18 +1,30 @@
 package GUI;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.Scanner;
 import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.InputVerifier;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
 import javax.swing.border.EtchedBorder;
 import javax.swing.text.JTextComponent;
 
-public class CreateCharacter {
+public class CreateCharacter implements ActionListener{
+
     private JFrame FRAME = new JFrame("Create Character");
     private Border PANEL_BORDER = BorderFactory.createEtchedBorder(EtchedBorder.RAISED);
+    private Border TEXT_BORDER = BorderFactory.createEtchedBorder(EtchedBorder.LOWERED);
     private JPanel BASE;
+    private JPanel PRESET_PANEL;
+    private ButtonGroup PRESET_BUTTONS;
     private JPanel SET_SPEED;
     private int SPEED = 1;
     private JTextField SPEED_DISPLAY;
@@ -28,53 +40,89 @@ public class CreateCharacter {
     private JPanel SET_DEXTERITY;
     private int DEXTERITY = 1;
     private JTextField DEXTERITY_DISPLAY;
-    
+
     public CreateCharacter() {
         formatAllStats();
+        formatPresets();
         formatFrame();
     }
     
-    private void formatAllStats() {
-        SET_SPEED = new JPanel();
-        SPEED_DISPLAY = new JTextField(1);
-        formatStatPanel(1,1,SET_SPEED,SPEED_DISPLAY,SPEED);
+    private void formatPresets() {
+        PRESET_PANEL = new JPanel();
+        PRESET_PANEL.setBounds(100, 2, 100, 200);
+        PRESET_PANEL.setBorder(BorderFactory.createTitledBorder(PANEL_BORDER, "Presets"));
         
-        SET_ENDURANCE = new JPanel();
-        ENDURANCE_DISPLAY = new JTextField(1);
-        formatStatPanel(1,52,SET_ENDURANCE,ENDURANCE_DISPLAY,ENDURANCE);
+        PRESET_BUTTONS = new ButtonGroup();
+        File presetFile = new File("src/GUI/ClassPresets.tgs");
+        JRadioButton currentButton = new JRadioButton("Knight");
         
-        SET_STRENGTH = new JPanel();
-        STRENGTH_DISPLAY = new JTextField(1);
-        formatStatPanel(1,103,SET_STRENGTH,STRENGTH_DISPLAY,STRENGTH);
-        
-        SET_INTELLIGENCE = new JPanel();
-        INTELLIGENCE_DISPLAY = new JTextField(1);
-        formatStatPanel(1,154,SET_INTELLIGENCE,INTELLIGENCE_DISPLAY,INTELLIGENCE);
-        
-        SET_DEXTERITY = new JPanel();
-        DEXTERITY_DISPLAY = new JTextField(1);
-        formatStatPanel(1,205,SET_DEXTERITY,DEXTERITY_DISPLAY,DEXTERITY);
+        PRESET_BUTTONS.add(currentButton);
+        currentButton.addActionListener(this);
+        PRESET_PANEL.add(currentButton);
     }
     
-    private void formatStatPanel(int x, int y, JPanel panel, JTextField display, int stat) {
-        panel.setBounds(x, y, 150, 50);
-        panel.setBorder(PANEL_BORDER);
-        
-        display.setEditable(false);
+    private String getLineElement(String line) {
+        String output = "";
+        Scanner lineScanner = new Scanner(line);
+        boolean first = true;
+        while (lineScanner.hasNext()) {
+            String current = lineScanner.next();
+            //System.out.println(current);
+            if (!current.contains("*") && first) {
+                output += current;
+                first = false;
+            } else if (!current.contains("*")) {
+                output += " " + current;
+            }
+        }
+        //System.out.println(output);
+        return output;
+    }
+
+    private void formatAllStats() {
+        int displaySize = 1;
+
+        SET_SPEED = new JPanel();
+        SPEED_DISPLAY = new JTextField(displaySize);
+        formatStatPanel(2, 2, SET_SPEED, SPEED_DISPLAY, SPEED, "Speed");
+
+        SET_ENDURANCE = new JPanel();
+        ENDURANCE_DISPLAY = new JTextField(displaySize);
+        formatStatPanel(2, 53, SET_ENDURANCE, ENDURANCE_DISPLAY, ENDURANCE, "Endurance");
+
+        SET_STRENGTH = new JPanel();
+        STRENGTH_DISPLAY = new JTextField(displaySize);
+        formatStatPanel(2, 104, SET_STRENGTH, STRENGTH_DISPLAY, STRENGTH, "Strength");
+
+        SET_INTELLIGENCE = new JPanel();
+        INTELLIGENCE_DISPLAY = new JTextField(displaySize);
+        formatStatPanel(2, 155, SET_INTELLIGENCE, INTELLIGENCE_DISPLAY, INTELLIGENCE, "Intelligence");
+
+        SET_DEXTERITY = new JPanel();
+        DEXTERITY_DISPLAY = new JTextField(displaySize);
+        formatStatPanel(2, 206, SET_DEXTERITY, DEXTERITY_DISPLAY, DEXTERITY, "Dexterity");
+    }
+
+    private void formatStatPanel(int x, int y, JPanel panel, JTextField display, int stat, String statName) {
+        panel.setBounds(x, y, 85, 50);
+        panel.setBorder(BorderFactory.createTitledBorder(PANEL_BORDER, statName));
+
         display.setText(stat + "");
         display.setBackground(Color.WHITE);
-        
+
         panel.add(display);
     }
-    
-    private void formatFrame() { 
+
+    private void formatFrame() {
         FRAME.add(SET_SPEED);
         FRAME.add(SET_ENDURANCE);
         FRAME.add(SET_STRENGTH);
         FRAME.add(SET_INTELLIGENCE);
         FRAME.add(SET_DEXTERITY);
-        
+        FRAME.add(PRESET_PANEL);
+
         BASE = new JPanel();
+        BASE.setBorder(PANEL_BORDER);
         FRAME.add(BASE);
 
         FRAME.setSize(300, 300);
@@ -83,5 +131,14 @@ public class CreateCharacter {
         FRAME.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //PLAYER_NAME.requestFocusInWindow();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        Object performer = ae.getSource();
+        if(performer instanceof JRadioButton) {
+            JRadioButton button = (JRadioButton)(performer);
+            System.out.println(button.getLabel());
+        }
     }
 }
